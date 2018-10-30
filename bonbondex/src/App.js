807f2d584e6bce925e16a2ad6101b83b2ead4605
Christopher ADAMS <CJ.ADAMS@outlook.com>
2018-10-30 14:33:19 +0100

    Ajout de la navbar

--- bonbondex/src/App.js
+++ bonbondex/src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import NavbarFeatures from './components/NavbarFeatures';
+
 import './App.css';
-import { Button } from 'mdbreact';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-<Button color="secondary">Secondary</Button>
+        <NavbarFeatures />
       </div>
     );
   }
