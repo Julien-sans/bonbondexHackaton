097c411513d7b2344d40b3793a1367a6ebe17df4
Eva Spessotto <eva@spessotto.com>
2018-10-30 17:08:54 +0100

    Modal unique pour chaque carte

--- bonbondex/src/components/Home.js
+++ bonbondex/src/components/Home.js
@@ -1,21 +1,24 @@
 import React, { Component, Fragment } from 'react';
 import { Container, Row, Col, Button, Card, CardBody, CardImage, CardTitle, Modal, ModalBody, ModalHeader, ModalFooter } from 'mdbreact';
 import  '../data/liste_bonbons';
+import '../style/Home.scss';
 
 
 class Home extends Component {
     constructor(props){
         super(props);
         this.state = {
-            modal: false
+            modal4601798030055: false
         }
     }
 
-    toggle = () => {
+    toggle(nr) {
+        let modalNumber = 'modal' + nr
         this.setState({
-          modal: !this.state.modal
+          [modalNumber]: !this.state[modalNumber]
         });
-      }
+    }
+
     render() {
   
         return (
@@ -24,25 +27,33 @@ class Home extends Component {
                     <Row >
                         {
                             this.props.listeBonbons.map((bonbon, key) => {
-                                const { product_name, image_front_small_url } = bonbon;
+                                const { product_name, image_front_small_url, id, allergens } = bonbon;
+                                let modalNumber = 'modal' + id
                                 return(
-                                    <Col lg="4" md="6" xs="12">
-                                        <Card className="mt-3" onClick={this.toggle.bind(this)}>
-                                            <CardImage onClick={this.toggle.bind(this)} className="img-fluid w-100 " src={image_front_small_url} />
+                                    <Col key={key} lg="4" md="6" xs="12">
+                                        <Card className="mt-3" onClick={() => this.toggle(id)} style={{height: "300px", cursor: "pointer"}}>
+                                            <CardImage className="img-fluid w-100" src={image_front_small_url}  />
                                             <CardBody className="p-2" style={{fontSize: "1.2em"}}>
                                                 <CardTitle >{product_name}</CardTitle>
                                             </CardBody>
                                         </Card>
-                                        <Modal isOpen={this.state.modal} toggle={this.toggle} size="fluid" full-height position="bottom">
-                                            <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
+                                        <Modal isOpen={this.state[modalNumber]} size="lg" lg position="top">
+                                            <ModalHeader toggle={() => this.toggle(id)}></ModalHeader>
                                             <ModalBody>
-                                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                                <Container>
+                                                    <Row>
+                                                        <Col xs="12">
+                                                            <img className="img-fluid" src={image_front_small_url} /> 
+                                                        </Col>
+                                                        <Col xs="12" >
+                                                            <h2>{product_name}</h2>
+                                                            <p>Liste des alergènes: {allergens}</p>
+                                                        </Col>
+                                                    </Row>
+                                                </Container>
                                             </ModalBody>
-                                            <ModalFooter>
-                                                <Button color="secondary" onClick={this.toggle}>Close</Button>
-                                                <Button color="primary">Save changes</Button>
-                                            </ModalFooter>
                                         </Modal>
+                                        
                                     </Col>
                                    
                                 )
