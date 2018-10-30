038c64094d634b65be0363808782e9287623a0ba
Christopher ADAMS <CJ.ADAMS@outlook.com>
2018-10-30 15:26:55 +0100

    centered searchbar

--- bonbondex/src/components/Searchbar.js
+++ bonbondex/src/components/Searchbar.js
@@ -7,7 +7,7 @@ class Searchbar extends React.Component {
         <div class="mt-5"></div>
         <form id="searchbar" className="form-inline active-cyan-3 active-cyan-4 mb-4 col-8 mx-auto">
           <i className="fa fa-search" aria-hidden="true"></i>
-          <input className="form-control form-control-sm ml-3 w-75" type="text" placeholder="Search" aria-label="Search" />
+          <input className="form-control form-control-sm ml-3 w-75 mx-auto" type="text" placeholder="Search" aria-label="Search" />
         </form>
       </div>
 
