0395fd7119e48b224392af93c555ade60efd0cf2
Julien SANS <julien_ss@hotmail.fr>
2018-10-30 16:31:48 +0100

    ajout des routes

--- bonbondex/src/App.js
+++ bonbondex/src/App.js
@@ -2,7 +2,8 @@ import React, { Component, Fragment } from 'react';
 import './App.css';
 import Home from './components/Home';
 import NavbarFeatures from './components/NavbarFeatures';
-
+import MesBonbons from './components/MesBonbons.js';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
@@ -12,11 +13,15 @@ class App extends Component {
     const listeBonbons = donneesBonbons.products
     console.log(listeBonbons)
     return (
-      <Fragment>
-        <NavbarFeatures />
-        <Home />
-      </Fragment>
-
+        <BrowserRouter>
+          <Fragment>
+            <NavbarFeatures/>
+            <Switch>
+              <Route exact path="(/|/home)" component={Home} />
+              <Route exact path="/mes-bonbons" component={MesBonbons} />
+            </Switch>
+          </Fragment>
+        </BrowserRouter>
     );
   }
 }
