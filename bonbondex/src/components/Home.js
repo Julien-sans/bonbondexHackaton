4320866353c971d5b2f42c384a2d1cb60eec5c60
Christopher ADAMS <CJ.ADAMS@outlook.com>
2018-10-31 09:37:11 +0100

    fix bug ajout du meme bonbon plusieurs fois dans Mes Bonbons

--- bonbondex/src/components/Home.js
+++ bonbondex/src/components/Home.js
@@ -1,12 +1,12 @@
 import React, { Component, Fragment } from 'react';
 import { Container, Row, Col, Button, Card, CardBody, CardImage, CardTitle, CardFooter, Modal, ModalBody, ModalHeader, Fa } from 'mdbreact';
-import  '../data/liste_bonbons';
+import '../data/liste_bonbons';
 import '../style/Home.scss';
 import BonbonCard from './BonbonCard';
 
 
 class Home extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       modal4601798030055: false,
@@ -18,9 +18,9 @@ class Home extends Component {
 
   componentDidMount() {
     localStorage.getItem('list') &&
-    this.setState({
-      list: JSON.parse(localStorage.getItem('list')),
-    })
+      this.setState({
+        list: JSON.parse(localStorage.getItem('list')),
+      })
   }
 
   addItem = event => {
@@ -29,6 +29,9 @@ class Home extends Component {
 
   handleClick = bonbon => {
     const { list } = this.state;
+    if (list.includes(bonbon)) {
+      return
+    }
     const newList = [...list, bonbon]
     localStorage.setItem('list', JSON.stringify(newList));
     this.setState({
@@ -42,12 +45,11 @@ class Home extends Component {
         <Container >
           <Row >
             {
-              this.props.listeBonbons.map((bonbon, key) =>
-              {
+              this.props.listeBonbons.map((bonbon, key) => {
                 const bonbonInList = this.state.list.find((bonbonFound) => bonbon.id === bonbonFound.id)
                 const bonbonFound = bonbonInList !== undefined
-                return <BonbonCard bonbonFound={bonbonFound} key={key} handleClick={this.handleClick} bonbon={bonbon}/>
-              } )
+                return <BonbonCard bonbonFound={bonbonFound} key={key} handleClick={this.handleClick} bonbon={bonbon} />
+              })
             }
           </Row>
         </Container>
